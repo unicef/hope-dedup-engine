d7efbea54ebb4e636c12a10ac5221f6468bb91a3
Domenico DiNicola <dom.dinicola@gmail.com>
2024-05-08 17:29:53 +0200

    add hope storage

---
+++ src/hope_dedup_engine/apps/hope/models/_base.py
@@ -0,0 +1,36 @@
+from collections.abc import Iterable
+from typing import Any
+
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True, db_index=True)
+
+    class Meta:
+        abstract = True
+
+
+class HopeModel(models.Model):
+    # id = models.CharField(primary_key=True, max_length=100, editable=False)
+
+    class Meta:
+        abstract = True
+        managed = False
+        app_label = "hope"
+
+    class Tenant:
+        tenant_filter_field = None
+
+    def save(
+        self,
+        force_insert: bool = ...,
+        force_update: bool = ...,
+        using: str | None = ...,
+        update_fields: Iterable[str] | None = ...,
+    ) -> None:
+        pass
+
+    def delete(self, using: Any = None, keep_parents: bool = False) -> tuple[int, dict[str, int]]:
+        return 0, {}
