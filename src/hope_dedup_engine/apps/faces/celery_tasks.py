397668461ecc2f0dfc92684d53e3e97a22fb6ef0
Vitali Yanushchyk <vitali.yanushchyk@valor-software.com>
2024-05-20 05:25:47 -0400

    chg ! use DeduplicationSet pk as input to deduplication pipeline

--- src/hope_dedup_engine/apps/faces/celery_tasks.py
+++ src/hope_dedup_engine/apps/faces/celery_tasks.py
@@ -1,13 +1,17 @@
+from uuid import UUID
+
 from celery import shared_task
 
 from hope_dedup_engine.apps.faces.utils.celery_utils import task_lifecycle
 from hope_dedup_engine.apps.faces.utils.duplication_detector import DuplicationDetector
+from hope_dedup_engine.apps.public_api.models import DeduplicationSet
 
 
 @shared_task(bind=True, soft_time_limit=0.5 * 60 * 60, time_limit=1 * 60 * 60)
 @task_lifecycle(name="Deduplicate", ttl=1 * 60 * 60)
-# TODO: Use DeduplicationSet objects as input to deduplication pipeline
-def deduplicate(self, filename: str):
-    # deduplicate.delay(filename=filename)
-    dd = DuplicationDetector(filename)
-    return dd.find_duplicates()
+def deduplicate(self, pk: UUID) -> set[str]:
+    duplicates = set()
+    for filename in DeduplicationSet.objects.get(pk=pk).image_set.values_list("filename", flat=True):
+        dd = DuplicationDetector(filename)
+        duplicates.update(dd.detect_duplicates())
+    return duplicates
