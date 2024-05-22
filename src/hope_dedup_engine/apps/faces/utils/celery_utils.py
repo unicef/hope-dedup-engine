b2afe9c6de3a9cddbe09e5d61bd7c0c0560dd7a9
Vitali Yanushchyk <vitali.yanushchyk@valor-software.com>
2024-05-22 15:31:46 -0400

    del ! TaskModel

--- src/hope_dedup_engine/apps/faces/utils/celery_utils.py
+++ src/hope_dedup_engine/apps/faces/utils/celery_utils.py
@@ -2,11 +2,8 @@ import logging
 from functools import wraps
 
 from django.conf import settings
-from django.utils import timezone
 
 import redis
-
-from hope_dedup_engine.apps.faces.models import TaskModel
 
 redis_client = redis.Redis.from_url(settings.CELERY_BROKER_URL)
 
@@ -17,6 +14,7 @@ def task_lifecycle(name: str, ttl: int):
         def wrapper(self, *args, **kwargs):
             logger = logging.getLogger(func.__module__)
             logger.info(f"{name} task started")
+            result = None
 
             filename: str = args[0] if args else kwargs.get("filename")
             lock_name: str = f"{name}_{filename}"
@@ -24,27 +22,13 @@ def task_lifecycle(name: str, ttl: int):
                 logger.info(f"Task {name} with brocker lock {lock_name} is already running.")
                 return None
 
-            task: TaskModel = None
-            result = None
-
             try:
-                task = TaskModel.objects.create(name=name, celery_task_id=self.request.id)
                 result = func(self, *args, **kwargs)
-                task.status = TaskModel.StatusChoices.COMPLETED_SUCCESS
-                task.completed_at = timezone.now()
-                task.is_success = True
             except Exception as e:
                 logger.exception(f"{name} task failed", exc_info=e)
-                if task:
-                    task.status = TaskModel.StatusChoices.FAILED
-                    task.completed_at = timezone.now()
-                    task.is_success = False
-                    task.error = str(e)
                 raise e
             finally:
                 _release_lock(lock_name)
-                if task:
-                    task.save(update_fields=["status", "completed_at", "is_success", "error"])
                 logger.info(f"{name} task ended")
             return result
 
