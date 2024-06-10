c3eae1b75522e850711472df023cc1779af7c1fa
sergey-misuk-valor <168101676+sergey-misuk-valor@users.noreply.github.com>
2024-06-10 14:48:17 +0400

    Register API models in admin (#27)

--- tests/extras/testutils/factories/__init__.py
+++ tests/extras/testutils/factories/__init__.py
@@ -1,3 +1,8 @@
+import importlib
+import pkgutil
+from pathlib import Path
+
+from factory.django import DjangoModelFactory
 from pytest_factoryboy import register
 
 from .base import AutoRegisterModelFactory, TAutoRegisterModelFactory, factories_registry
@@ -6,13 +11,22 @@ from .social import SocialAuthUserFactory  # noqa
 from .user import ExternalSystemFactory, GroupFactory, SuperUserFactory, User, UserFactory  # noqa
 from .userrole import UserRole, UserRoleFactory  # noqa
 
+for _, name, _ in pkgutil.iter_modules([str(Path(__file__).parent)]):
+    importlib.import_module(f".{name}", __package__)
 
-def get_factory_for_model(_model) -> type[TAutoRegisterModelFactory]:
+
+django_model_factories = {factory._meta.model: factory for factory in DjangoModelFactory.__subclasses__()}
+
+
+def get_factory_for_model(_model) -> type[TAutoRegisterModelFactory] | type[DjangoModelFactory]:
     class Meta:
         model = _model
 
     bases = (AutoRegisterModelFactory,)
     if _model in factories_registry:
         return factories_registry[_model]  # noqa
+
+    if _model in django_model_factories:
+        return django_model_factories[_model]
 
     return register(type(f"{_model._meta.model_name}AutoCreatedFactory", bases, {"Meta": Meta}))  # noqa
