fd743acb06251a3c2295585b47f5b89de2e9b02e
Vitali Yanushchyk <vitali.yanushchyk@valor-software.com>
2024-10-22 03:40:58 -0300

    chg ! admin panel - process deduplication set - is root

--- src/hope_dedup_engine/config/fragments/root.py
+++ src/hope_dedup_engine/config/fragments/root.py
@@ -1,4 +1,4 @@
 from hope_dedup_engine.config import env
 
-ROOT_TOKEN = env("ROOT_TOKEN")
-ROOT_TOKEN_HEADER = env("ROOT_TOKEN_HEADER", default="x-root-token")
+ROOT_TOKEN = env("ROOT_ACCESS_TOKEN")
+ROOT_TOKEN_HEADER = env("ROOT_TOKEN_HEADER")
