47fbe997eb8edd4441337886d33950b412ce9886
Emil Matyjaszewski <emil.matyjaszewski@kellton.com>
2025-01-09 13:12:27 +0100

    Add --no-sync-models flag to setup in entrypoint

--- docker/bin/docker-entrypoint.sh
+++ docker/bin/docker-entrypoint.sh
@@ -29,7 +29,7 @@ echo "Docker run command: $1"
 case "$1" in
     setup)
       django-admin check --deploy || exit 1
-      django-admin upgrade --no-static || exit 1
+      django-admin upgrade --no-static --no-sync-models || exit 1
       exit 0
       ;;
     worker)
