494080477bd9a4bd20b025d4f52c8578ceda2240
Sergey Misuk <sergey.misuk@valor-software.com>
2025-01-25 10:28:11 +0300

    Apply some refactoring

--- src/hope_dedup_engine/types.py
+++ src/hope_dedup_engine/types.py
@@ -1,3 +1,6 @@
+from collections.abc import Iterable
+from typing import Self
+
 from hope_dedup_engine.constants import FacialError
 
 ReferencePK = str
@@ -12,4 +15,10 @@ ImageEmbedding = tuple[Filename, Embedding]
 ImageEmbeddingError = tuple[Filename, FacialError]
 Finding = tuple[ReferencePK, ReferencePK, Score]
 
-IgnoredPair = tuple[ReferencePK, ReferencePK]
+
+class SortedTuple(tuple):
+    def __new__(cls, iterable: Iterable) -> Self:
+        return tuple.__new__(cls, sorted(iterable))
+
+
+IgnoredPair = SortedTuple[ReferencePK, ReferencePK]
