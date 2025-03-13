d3ec5ab016b4ae448157916125dbe45e43e0f459
Sergey Misuk <sergey.misuk@valor-software.com>
2025-03-13 14:48:24 +0300

    Do not list all files in storage

--- src/hope_dedup_engine/apps/faces/managers/storage.py
+++ src/hope_dedup_engine/apps/faces/managers/storage.py
@@ -1,22 +1,13 @@
-from fnmatch import fnmatch
-from typing import Final
-
 from django.conf import settings
 
 import cv2
 import numpy as np
 from storages.backends.azure_storage import AzureStorage
 
-FILES_PATTERN: Final[tuple[str]] = ("*.png", "*.jpg", "*.jpeg")
-
 
 class ImagesStorageManager:
     def __init__(self) -> None:
         self.storage: AzureStorage = AzureStorage(**settings.STORAGES.get("hope").get("OPTIONS"))
-
-    def get_files(self, pattern: tuple = FILES_PATTERN) -> list[str]:
-        _, images = self.storage.listdir("")
-        return [f for f in images if any(fnmatch(f, p) for p in pattern)]
 
     def load_image(self, file: str) -> np.ndarray:
         with self.storage.open(file, "rb") as img_file:
