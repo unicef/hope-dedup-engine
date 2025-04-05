c058ae06a4e415f157618789d8018be08ce11e50
akshansh <akshansh@debian>
2025-04-06 01:01:42 +0530

    generated tokens

--- src/hope_dedup_engine/apps/api/models/auth.py
+++ src/hope_dedup_engine/apps/api/models/auth.py
@@ -10,3 +10,8 @@ class HDEToken(Token):
     """
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="auth_tokens", on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = secrets.token_hex(20)
+        super().save(*args, **kwargs)
