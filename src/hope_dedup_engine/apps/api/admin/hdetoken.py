4b95cf6909227e01766e6468cb948ad593d14576
akshansh <akshansh@debian>
2025-04-11 12:54:41 +0530

    Token generated: key message

--- src/hope_dedup_engine/apps/api/admin/hdetoken.py
+++ src/hope_dedup_engine/apps/api/admin/hdetoken.py
@@ -1,8 +1,18 @@
 from django.contrib.admin import ModelAdmin, register
+from django.contrib import messages
 
 from hope_dedup_engine.apps.api.models import HDEToken
 
 
 @register(HDEToken)
 class HDETokenAdmin(ModelAdmin):
-    list_display = ("key", "user")
+    list_display = ("user",)
+    
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        if not change:  # Only show the message when a new token is created
+            self.message_user(
+                request, 
+                f"Token generated: {obj.key}", 
+                messages.SUCCESS
+            )
