eeeee6d68f0015e57c0bc597faa9cca4486e8a98
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2025-08-01 21:53:16 +0200

    CACHE_URL set

--- tests/apps/faces/test_utils.py
+++ tests/apps/faces/test_utils.py
@@ -9,8 +9,8 @@ from hope_dedup_engine.apps.faces.utils import is_facial_error, report_long_exec
 @pytest.mark.parametrize(
     ("time_side_effect", "should_be_called"),
     [
-        ([0, 1], False),  # 1 second duration, less than threshold
-        ([0, 6], True),  # 6 seconds duration, more than threshold
+        ([0, 1], False),
+        ([0, 6], True),
     ],
 )
 @patch("hope_dedup_engine.apps.faces.utils.sentry_sdk")
@@ -19,7 +19,7 @@ def test_report_long_execution(mock_sentry, time_side_effect, should_be_called, 
     settings.DEFAULT_THRESHOLD_SECONDS = 5
     with patch("time.time", side_effect=time_side_effect):
         with report_long_execution("Test Task"):
-            pass  # Simulate some work
+            pass
 
     if should_be_called:
         mock_sentry.capture_message.assert_called_once()
@@ -33,12 +33,10 @@ def test_report_long_execution(mock_sentry, time_side_effect, should_be_called, 
 @pytest.mark.parametrize(
     ("value", "expected"),
     [
-        # Success cases (not errors)
         (Image.StatusCode.DEDUPLICATE_SUCCESS, False),
         (Image.StatusCode.DEDUPLICATE_SUCCESS.name, False),
         (Image.StatusCode.DEDUPLICATE_SUCCESS.label, False),
         (200, False),
-        # Error cases
         (Image.StatusCode.NO_FILE_FOUND, True),
         (Image.StatusCode.NO_FILE_FOUND.name, True),
         (Image.StatusCode.NO_FILE_FOUND.label, True),
@@ -49,7 +47,6 @@ def test_report_long_execution(mock_sentry, time_side_effect, should_be_called, 
         (429, True),
         (Image.StatusCode.GENERIC_ERROR, True),
         (500, True),
-        # Invalid values
         ("some_random_string", False),
         (999, False),
         (None, False),
