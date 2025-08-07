f3120a99a0cb993039be4a99558a63c348417750
Domenico DiNicola <dom.dinicola@gmail.com>
2025-08-08 00:09:42 +0200

    fix

--- src/hope_dedup_engine/web/context_processors.py
+++ src/hope_dedup_engine/web/context_processors.py
@@ -3,7 +3,6 @@ from typing import Any
 
 from django.http import HttpRequest
 
-from hope_dedup_engine import VERSION
 from hope_dedup_engine.state import state
 
 
@@ -11,7 +10,6 @@ def current_state(request: HttpRequest) -> dict[str, Any]:
     return {
         "state": state,
         "app": {
-            "version": VERSION,
             "build_date": os.environ.get("BUILD_DATE", ""),
             "commit": os.environ.get("SOURCE_COMMIT", "-"),
         },
