6cbf358b06c8abbf306b1c8cbbe604806061c300
Sergey Misuk <sergey.misuk@valor-software.com>
2025-09-30 17:09:37 +0400

    Compare images between chunks

--- src/hope_dedup_engine/apps/faces/celery_tasks.py
+++ src/hope_dedup_engine/apps/faces/celery_tasks.py
@@ -1,4 +1,6 @@
 import traceback
+from itertools import combinations
+
 import sentry_sdk
 from functools import partial
 from typing import TYPE_CHECKING, Any, Iterable
@@ -88,7 +90,8 @@ def encode_chunk(
 @app.task(bind=True, base=DedupeTask)
 def dedupe_chunk(
     self: Task,
-    files: list[str],
+    files0: list[str],
+    files1: list[str],
     config: dict[str, Any],
 ) -> FindingType:
     """Deduplicate faces in a chunk of files."""
@@ -98,7 +101,8 @@ def dedupe_chunk(
         encoded = ds.get_encodings()
         ignored_pairs = set(ds.get_ignored_pairs())
         return dedupe_images(
-            files,
+            files0,
+            files1,
             encoded,
             ignored_pairs,
             dedupe_threshold=config.get("deduplicate", {}).get("threshold"),
@@ -170,7 +174,7 @@ def deduplicate_dataset(
     ds = DeduplicationSet.objects.get(pk=config.get("deduplication_set_id"))
     try:
         chunks = get_chunks(ds.get_encodings().keys(), purpose=ChunkPurpose.DEDUPE)
-        tasks = [dedupe_chunk.s(chunk, config) for chunk in chunks]
+        tasks = [dedupe_chunk.s(chunk0, chunk1, config) for chunk0, chunk1 in combinations(chunks, 2)]
         chord_id = chord(tasks)(callback_findings.s(config=config))
         return {
             "deduplication_set": str(ds),
