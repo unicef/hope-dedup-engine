7f2f4d5693c8bd04d3ddb4ac43f9fbd49b17fe37
patsatsia <giorgi.patsatsia@valor-software.com>
2025-09-30 21:11:04 +0400

    Alter "DedupJob" model class
    Switch from "OneToOneField" to "ForeignKey" for "deduplication_set"

--- src/hope_dedup_engine/apps/api/models/jobs.py
+++ src/hope_dedup_engine/apps/api/models/jobs.py
@@ -4,8 +4,19 @@ from django_celery_boost.models import CeleryTaskModel
 
 
 class DedupJob(CeleryTaskModel):
-    deduplication_set = models.OneToOneField("DeduplicationSet", on_delete=models.CASCADE)
+    deduplication_set = models.ForeignKey(
+        "DeduplicationSet",
+        on_delete=models.CASCADE,
+        related_name="dedup_jobs",
+    )
     serialized_lock = models.CharField(max_length=128, null=True, editable=False)
     progress = models.IntegerField(default=0)
+    files_data = models.JSONField(null=True, editable=False)
 
     celery_task_name = "hope_dedup_engine.apps.api.deduplication.process.find_duplicates"
+
+    def get_filenames(self) -> list[str]:
+        if not self.files_data:
+            return []
+
+        return [f.get("filename") for f in self.files_data]
