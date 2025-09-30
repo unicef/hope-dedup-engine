cb8cd2f154571434d154316e68291487be4f6705
patsatsia <giorgi.patsatsia@valor-software.com>
2025-09-30 21:17:17 +0400

    Alter test cases according to newly implemented changes

--- tests/api/test_deduplication_set_process.py
+++ tests/api/test_deduplication_set_process.py
@@ -1,38 +1,69 @@
-from unittest.mock import MagicMock
+from typing import List
 
 import pytest
+from api.api_const import DEDUPLICATION_SET_PROCESS_VIEW
+from api.api_const import JSON
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
 
-from api.api_const import DEDUPLICATION_SET_PROCESS_VIEW
 from hope_dedup_engine.apps.api.models import DeduplicationSet
-from hope_dedup_engine.apps.api.utils.process import AlreadyProcessingError
+
+
+def test_process_initialization_with_empty_data(
+    api_client: APIClient,
+    deduplication_set: DeduplicationSet,
+) -> None:
+    response = api_client.post(reverse(DEDUPLICATION_SET_PROCESS_VIEW, (deduplication_set.pk,)))
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert "non_field_errors" in response.json()
+
+
+def test_process_initialization_with_empty_list(
+    api_client: APIClient,
+    deduplication_set: DeduplicationSet,
+) -> None:
+    response = api_client.post(reverse(DEDUPLICATION_SET_PROCESS_VIEW, (deduplication_set.pk,)), data=[], format=JSON)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert "non_field_errors" in response.json()
 
 
 @pytest.mark.parametrize(
-    "deduplication_set__state",
+    ("data", "exception_key"),
     [
-        DeduplicationSet.State.READY,
-        DeduplicationSet.State.MODIFIED,
-        DeduplicationSet.State.FAILED,
+        ([{"reference_pk": "12345678"}], "filename"),
+        ([{"filename": "some_sick_filename"}], "reference_pk"),
     ],
 )
-def test_can_trigger_deduplication_set_processing_in_non_processing_state(
+def test_process_initialization_with_invalid_data(
     api_client: APIClient,
-    start_processing: MagicMock,
     deduplication_set: DeduplicationSet,
-) -> None:
-    response = api_client.post(reverse(DEDUPLICATION_SET_PROCESS_VIEW, (deduplication_set.pk,)))
+    data: List[JSON],
+    exception_key: str,
+):
+    response = api_client.post(reverse(DEDUPLICATION_SET_PROCESS_VIEW, (deduplication_set.pk,)), data=data, format=JSON)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert exception_key in response.json()[0]
+
+
+def test_process_initialization(
+    api_client: APIClient,
+    deduplication_set: DeduplicationSet,
+):
+    data = [
+        {
+            "reference_pk": "12345678",
+            "filename": "filename",
+        },
+        {
+            "reference_pk": "87654321",
+            "filename": "filename2",
+        },
+    ]
+    response = api_client.post(reverse(DEDUPLICATION_SET_PROCESS_VIEW, (deduplication_set.pk,)), data=data, format=JSON)
     assert response.status_code == status.HTTP_200_OK
-    start_processing.assert_called_once_with(deduplication_set)
+    assert response.json() == {"message": "started"}
 
-
-def test_cannot_trigger_deduplication_set_processing_when_already_processing(
-    api_client: APIClient,
-    start_processing: MagicMock,
-    deduplication_set: DeduplicationSet,
-) -> None:
-    start_processing.side_effect = AlreadyProcessingError
-    response = api_client.post(reverse(DEDUPLICATION_SET_PROCESS_VIEW, (deduplication_set.pk,)))
-    assert response.status_code == status.HTTP_409_CONFLICT
+    deduplication_set.refresh_from_db()
+    dedup_job = deduplication_set.dedup_jobs.last()
+    assert dedup_job.files_data == data
