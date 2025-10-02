cb2762ee269b2f2348f46f94974b714313689c07
Vitali Yanushchyk <vitali.yanushchyk@valor-software.com>
2025-10-02 10:21:55 -0300

    chg ! chunk size

--- src/hope_dedup_engine/apps/faces/celery_tasks.py
+++ src/hope_dedup_engine/apps/faces/celery_tasks.py
@@ -26,7 +26,7 @@ if TYPE_CHECKING:
 
 class ChunkPurpose(IntEnum):
     ENCODE = 25
-    DEDUPE = 1000
+    DEDUPE = 25
 
 
 def get_chunks(filenames: Iterable[str], *, purpose: ChunkPurpose) -> list[list[str]]:
