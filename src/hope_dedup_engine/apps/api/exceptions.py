f8426a9c771abb3bd99af1bc46ba40107453532a
patsatsia <giorgi.patsatsia@valor-software.com>
2025-10-03 15:06:41 +0400

    Alter "TooManyReferencePksException" class
    Override __init__ method to include constant value in the message

--- src/hope_dedup_engine/apps/api/exceptions.py
+++ src/hope_dedup_engine/apps/api/exceptions.py
@@ -5,4 +5,8 @@ from constance import config
 
 class TooManyReferencePksException(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
-    default_detail = f"Too many reference pks. Max number allowed: {config.MAX_REFERENCE_PKS_ALLOWED_FOR_FINDINGS}"
+
+    def __init__(self, detail=None, code=None):
+        if detail is None:
+            detail = f"Too many reference pks. Max number allowed: {config.MAX_REFERENCE_PKS_ALLOWED_FOR_FINDINGS}"
+        super().__init__(detail=detail, code=code)
