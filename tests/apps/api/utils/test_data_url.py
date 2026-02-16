9c3f5863daff04095a14820181677aee4231d3cb
Sergey Misuk <sergey.misuk@valor-software.com>
2026-02-17 02:10:35 +0400

    Increase coverage

--- tests/apps/api/utils/test_data_url.py
+++ tests/apps/api/utils/test_data_url.py
@@ -6,8 +6,17 @@ from hope_dedup_engine.apps.api.utils.data_url import ParsedDataURL, parse_data_
 @pytest.mark.parametrize(
     ("url", "expected"),
     [
-        ("", None),
         ("data:media-type;base64,data", ParsedDataURL(mimetype="media-type", encoding="base64", content="data")),
+        ("data:media-type;base64,", ParsedDataURL(mimetype="media-type", encoding="base64", content="")),
+        ("data:media-type;,data", ParsedDataURL(mimetype="media-type", encoding=None, content=",data")),
+        ("data:media-type;data", ParsedDataURL(mimetype="media-type", encoding=None, content="data")),
+        ("data:media-type;other,data", ParsedDataURL(mimetype="media-type", encoding=None, content="other,data")),
+        ("data:;base64,data", ParsedDataURL(mimetype=None, encoding="base64", content="data")),
+        (":media-type;base64,data", None),
+        ("other:media-type;base64,data", None),
+        (";base64,data", None),
+        ("data:media-type;", ParsedDataURL(mimetype="media-type", encoding=None, content="")),
+        ("", None),
     ],
 )
 def test_data_url(url: str, expected: ParsedDataURL | None) -> None:
