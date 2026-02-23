1fd38e1c4f896e31bacec42f2dd9aa18fa47f013
Sergey Misuk <sergey.misuk@valor-software.com>
2026-02-23 12:59:15 +0400

    Add send notification button

--- src/hope_dedup_engine/apps/api/utils/notification.py
+++ src/hope_dedup_engine/apps/api/utils/notification.py
@@ -9,9 +9,20 @@ from hope_dedup_engine.apps.api.models import DeduplicationSet
 REQUEST_TIMEOUT: Final[int] = 5
 
 
-def send_notification(deduplication_set: DeduplicationSet) -> None:
-    if not (deduplication_set.notify and deduplication_set.notification_url):
-        return
+class ErrorMessage(str):
+    pass
+
+
+class WarningMessage(str):
+    pass
+
+
+def send_notification(deduplication_set: DeduplicationSet, force: bool = False) -> None | ErrorMessage | WarningMessage:
+    if not deduplication_set.notification_url:
+        return WarningMessage("No notification URL configured.")
+
+    if not (deduplication_set.notify or force):
+        return WarningMessage("Notification disabled.")
 
     token = getattr(config, "HOPE_API_TOKEN", "")
     headers = {"Authorization": f"Token {token}"} if token else None
@@ -20,3 +31,4 @@ def send_notification(deduplication_set: DeduplicationSet) -> None:
         requests.get(deduplication_set.notification_url, headers=headers, timeout=REQUEST_TIMEOUT).raise_for_status()
     except requests.RequestException as e:
         sentry_sdk.capture_exception(e)
+        return ErrorMessage(f"Failed to send notification: {e}.")
