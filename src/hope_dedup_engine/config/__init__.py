d1e18acace7e3c62f80f02be77bc5f4f6b19ab5c
Domenico DiNicola <dom.dinicola@gmail.com>
2026-09-22 20:35:29 +0300

    Harden session cookie: 1-day lifetime, expire at browser close

--- src/hope_dedup_engine/config/__init__.py
+++ src/hope_dedup_engine/config/__init__.py
@@ -250,6 +250,20 @@ CONFIG: "dict[str, ConfigItem]" = {
         False,
         setting("session-cookie-httponly"),
     ),
+    "SESSION_COOKIE_AGE": (
+        int,
+        86400,
+        86400,
+        False,
+        setting("std-setting-SESSION_COOKIE_AGE"),
+    ),
+    "SESSION_EXPIRE_AT_BROWSER_CLOSE": (
+        bool,
+        True,
+        True,
+        False,
+        setting("std-setting-SESSION_EXPIRE_AT_BROWSER_CLOSE"),
+    ),
     "SESSION_COOKIE_NAME": (str, "dedupe_session", setting("session-cookie-name")),
     "SESSION_COOKIE_PATH": (str, "/", setting("session-cookie-path")),
     "SESSION_COOKIE_SECURE": (
